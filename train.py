894b223dfe6b3ac8de6522d284314afa1856698f
vikram-mupparthi <44016625+vikram-mupparthi@users.noreply.github.com>
2024-04-08 02:13:58 +0530

    Update train.py

--- train.py
+++ train.py
@@ -5,8 +5,8 @@ import numpy as np
 
 # file input
 df = pd.read_csv("data/train.csv")
-df = df[0:1500]
-X = df.drop(columns=[[0, 1, 2, 3, 4, 5]], axis = 1).to_numpy()
+df = df.sample(frac=0.5, random_state=42)
+X = df.drop(columns=['Disease']).to_numpy()
 y = df['Disease'].to_numpy()
 labels = np.unique(y)
 y = np.array([np.where(labels == x) for x in y]).flatten()
